13f3db1d88580f6ec51ec90f78685a06eabe2854
chrisl <chrisl@pageuppeople.com>
2018-08-29 12:46:04 +1000

    Initial commit

---
+++ app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"encoding/base64"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+)
+
+func main() {
+	//Set up our command-line arguments
+	region := flag.String("region", "ap-southeast-2", "The AWS region containing the key that the value was encrypted with")
+	encodedEncryptedValue := flag.String("value", "", "The value to decrypt")
+	flag.Parse()
+
+	if *encodedEncryptedValue == "" {
+		fmt.Println("Error: Argument -value is required")
+		return
+	}
+
+	sessionConfig := aws.Config{ Region: aws.String(*region) }
+	apiSession, _ := session.NewSession(&sessionConfig)
+	kmsService := kms.New(apiSession)
+
+	//Encrypted KMS strings are base-64 encoded, so we need to decode it before decrypting it
+	decodedEncryptedValue, _ := base64.StdEncoding.DecodeString(*encodedEncryptedValue)
+	
+	result, _ := kmsService.Decrypt(&kms.DecryptInput{ CiphertextBlob: decodedEncryptedValue })
+
+	fmt.Println(string(result.Plaintext))
+}
